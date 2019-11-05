6741aee7b59e63f06275afa10ef9919501f4bafb
Charissa Beaty <char.beaty11@gmail.com>
2019-11-05 13:23:52 -0700

    added functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,7 +20,37 @@ connection.connect(function(err) {
 function runSearch() {
     inquirer 
     .prompt({
-        name: "product",
+        name: "item_id",
+        type: "input",
         message: "What is the ID of the product(s) you would like to buy?"
     })
+    .then(function(answer) {
+        var query = "SELECT item_id FROM products WHERE ?"
+        connection.query(query, {item_id: answer.item_id }, function(err, res) {
+         for (var i = 0; i < res.length; i++) {
+             console.log("Item ID: " + res[i].item_id);
+         } 
+         runSearch();  
+        });
+    });
+  
+} 
+
+function itemQuantity() {
+    inquirer
+    .prompt({
+        name: "unit",
+        type: "input",
+        message: "How many units of the product(s) would you like to buy?"
+    })
+    .then(function(answer) {
+        var query = "SELECT stock_quantity FROM products WHERE ?"
+        connection.query(query, {stock_quantity: answer.stock_quantity}, function(err, res) {
+            for (var i = 0; i < res.length; i++) {
+                console.log("Units Chosen: " + res[i].stock_quanity);
+            }
+            runSearch();
+        });
+    });
 }
+itemQuantity();
