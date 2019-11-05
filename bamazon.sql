bfdf2ac1d58429e019545c789bf68569317a791a
Charissa Beaty <char.beaty11@gmail.com>
2019-11-04 18:49:41 -0700

    Added values to products table

--- bamazon.sql
+++ bamazon.sql
@@ -16,4 +16,25 @@ CREATE TABLE products (
 SELECT * FROM products;
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ()
+VALUES ("Rocket Dog", "Shoes", 39.98, 200),
+("Slim Fit Jeans", "Apparel", 21.00, 200),
+("Adidas Running Shoes", "Shoes", 120.00, 160),
+("Pampers Diapers", "Baby", 35.00, 180),
+("Tom's Toothpaste", "Personal Care", 8.00, 450),
+(""),
+(""),
+(""),
+(""),
+("");
+
+CREATE TABLE departments(
+    department_id INT AUTO-INCREMENT NOT NULL,
+    department_name VARCHAR(50) NOT NULL,
+    over_head_costs DECIMAL(10,2) NOT NULL,
+    primary key(department_id)
+);
+
+SELECT * FROM departments;
+
+INSERT INTO departments (department_name, over_head_costs)
+VALUES ("Shoes", )
